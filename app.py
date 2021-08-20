5bca5e247928c35c8da806dc6fb79a63b6c6b394
hasanalibalcioglu <h.alibalcioglu@gmail.com>
2021-08-20 12:36:18 +0300

    tweets changed

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ else:
 
 f = open('tweets.json')
 twitter = json.load(f)
+def clearTweet(text):
+    text = re.sub('http\S+', '', text, flags=re.IGNORECASE | re.MULTILINE)
+    text = re.sub('@\w+', '', text, flags=re.IGNORECASE | re.MULTILINE)
+    return text
 
 class Tosi(discord.Client):
     async def on_ready(self):
@@ -20,10 +24,17 @@ class Tosi(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if re.search(r'\btosi\b', message.content, flags=re.IGNORECASE | re.MULTILINE):
+        if re.search(r'(\s?tosi){2}', message.content, flags=re.IGNORECASE | re.MULTILINE):
+            choice = twitter['tweet']["1643"]
+            text = clearTweet(choice)
+            img = twitter['thumbnail']["1643"]
+            e = discord.Embed()
+            e.set_image(url=img)
+            return await message.channel.send(text, embed=e)
+
+        elif re.search(r'\btosi\b', message.content, flags=re.IGNORECASE | re.MULTILINE):
             choice = random.choice(list(twitter['tweet'].items()))
-            text = re.sub('http\S+', '', choice[1].lower(), flags=re.IGNORECASE | re.MULTILINE)
-            text = re.sub('@\w+', '', text, flags=re.IGNORECASE | re.MULTILINE)
+            text = clearTweet(choice[1])
             try:
                 img = twitter['thumbnail'][choice[0]]
             except:
